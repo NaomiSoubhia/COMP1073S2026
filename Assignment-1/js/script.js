1e9755b7f5cb9577d260da8ae191b23cac298f9d
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-05-28 10:44:42 -0400

    arrays

---
+++ Assignment-1/js/script.js
@@ -0,0 +1,14 @@
+
+//Variables
+
+let agentType = [ "Ghost", "Shadow", "Viper", "Nova", "Phantom"];
+
+let location = [ "Arctic Research Base", "Abandoned Space Station", "Underground Bunker", "Cyber City","Volcanic Island"];
+
+let weapon = ["Plasma Rifle","Laser Pistol","EMP Grenades","Nano Blade","Quantum Cannon"];
+
+let mission = ["Rescue the Scientist","Retrieve Secret Data","Disable Enemy Network","Protect the VIP","Destroy the Prototype"];
+
+let riskLevel = ["Low","Moderate","High","Extreme","Critical"];
+
+
