47a77d6a9552b53750f4a81ce348bd5e6a6ff4d2
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-05-27 22:14:01 -0400

    second condition = Warning: Low Resources

--- Lab-3/js/script.js
+++ Lab-3/js/script.js
@@ -22,21 +22,8 @@ if(health < 30 && enemyNearby === true){
 // ammo < 5 OR shield is false
 // Display:
 // "Warning: Low Resources"
-// 3. MID MISSION STATUS
-// If:
-// missionProgress is between 1 and 70
-// Display:
-// "Mission In Progress"
-// 4. HIGH PROGRESS STATUS
-// If:
-// missionProgress > 70 AND enemyNearby is false
-// Display:
-// "Approaching Mission Completion"
-// 5. MISSION COMPLETE
-// If:
-// missionProgress === 100
-// Display:
-// "Mission Complete Successfully"
-// 6. DEFAULT STATE
-// If none of the above conditions match:
-// "System Stable"
+
+else if(ammo < 5 || shield === false){
+    display.textContent="Warning: Low Resources";
+}
+
