8d6a52d2148d42d771513fcf490433234f13198e
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-11 10:16:48 -0400

    Display name and student ID with JavaScript

---
+++ Assignment-3/js/script.js
@@ -0,0 +1,5 @@
+const name = "Naomi Soubhia Doi";
+const studentId = "200645137";
+
+let naomiInfos =  document.getElementById("naomiInfos");
+naomiInfos.textContent = `Name: ${name} | Student ID: ${studentId}`;
