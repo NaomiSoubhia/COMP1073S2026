a07f83b3806f21938d2b1f845a0105c7ad9e21c9
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-18 14:43:11 -0400

    api connection with key

--- Assignment-4/js/script.js
+++ Assignment-4/js/script.js
@@ -1,3 +1,8 @@
 const infos = document.getElementById("naomiInfo");
 
 infos.innerHTML = "Student: Naomi Soubhia Doi - 200645137";
+
+
+const API_KEY = "AIzaSyAy8_o6Tpu9KIvHFD6Oaorerjtb6N2m77o";
+
+const url = `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${API_KEY}`;
