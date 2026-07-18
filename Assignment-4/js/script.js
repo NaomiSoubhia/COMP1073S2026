f1a531a308ce70b1c3124fc082074a1a47740045
NaomiSoubhia <soubhiadoinaomi@gmail.com>
2026-07-18 15:12:38 -0400

    book search html, main + footer

--- Assignment-4/js/script.js
+++ Assignment-4/js/script.js
@@ -2,7 +2,9 @@ const infos = document.getElementById("naomiInfo");
 
 infos.innerHTML = "Student: Naomi Soubhia Doi - 200645137";
 
-
+//Key
 const API_KEY = "AIzaSyAy8_o6Tpu9KIvHFD6Oaorerjtb6N2m77o";
 
-const url = `https://www.googleapis.com/books/v1/volumes?q=harry+potter&key=${API_KEY}`;
+//URL
+const API_URL = `https://www.googleapis.com/books/v1/volumes?q=`;
+
