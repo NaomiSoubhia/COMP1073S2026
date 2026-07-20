e476ca7367c5f78eb58be0bf6979eacc51e46771
Naomi Soubhia Doi <97617026+NaomiSoubhia@users.noreply.github.com>
2026-07-20 16:36:41 -0400

    Update script.js

--- Assignment-4/js/script.js
+++ Assignment-4/js/script.js
@@ -8,8 +8,6 @@ const API_KEY = "AIzaSyAy8_o6Tpu9KIvHFD6Oaorerjtb6N2m77o";
 //URL
 const API_URL = `https://www.googleapis.com/books/v1/volumes?q=`;
 
-
-
 // Search button element
 const button = document.getElementById("searchBtn");
 
